53a923ff24baed778d2aec684c82cdf4df515129
胖水龙 <2637418187@qq.com>
2022-09-19 09:22:45 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-
+import { $http } from '@escook/request-miniprogram'
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
 
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  console.log("开始");
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
